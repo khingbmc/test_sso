29889e165e285abed17c4fd7134aac0431cbce31
KhingNoii <ratanon.jan@gmail.com>
2019-06-06 15:19:14 +0700

    object user

--- ldap_test/sso/ad_test.py
+++ ldap_test/sso/ad_test.py
@@ -3,17 +3,22 @@ import ldap
 def main():
     con = ldap.initialize('ldap://35.240.208.53:389')
 
-    user_dn = "test3@etcg.com"
+    user_dn = r"test3@etcg.com"
     password = "q?fUuvodoc<O]{p"
 
-    criteria = "(&(objectClass=user)(sAMAccountName=username))"
+    query = "(&(objectClass=user)(sAMAccountName=khing))"
     attributes = ['displayName', 'company']
 
     try:
         con.simple_bind_s(user_dn, password)
-        res = con.search_s("OU=AD_USER,DC=etcg,DC=com", ldap.SCOPE_SUBTREE, '(objectClass=User)')
-        for dn, entry in res:
-            print(dn)
+
+        res = con.search_s("OU=AD_USER,DC=etcg,DC=com", ldap.SCOPE_SUBTREE, query)
+        # username, password = input(), input()
+        for i, j in res:
+            print(i)
+        # 'sAMAccountName':
+        # for dn, entry in res:
+        #     print(entry)
     except Exception as error:
         print(error)
 
